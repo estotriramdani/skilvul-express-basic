17cac640d27dfbe54bb4aa76c56ae9cd184b1df2
Esto Triramdani N <estolagi@gmail.com>
2023-11-09 20:01:22 +0700

    add dialectModule manually

--- src/models/index.js
+++ src/models/index.js
@@ -3,6 +3,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'mysql',
+  dialectModule: require('mysql2')
 });
 
 module.exports = sequelize;
